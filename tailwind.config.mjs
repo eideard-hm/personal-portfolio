c624a078035d3fd0d3e48f5e8446247947a40b18
Accesos-Web-Team <accesoswebteam@gmail.com>
2025-01-07 18:35:03 -0500

    Update color scheme to use primary and secondary colors across components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7dd3fc',
+        secondary: '#f97316',
+      },
+    },
+  },
+  plugins: [],
+};
